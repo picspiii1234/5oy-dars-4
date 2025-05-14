220be1c15b9f14a9e5ba0ddb0d631c545fcd356d
picspiii1234 <ibrihim539@gmail.com>
2025-05-14 10:16:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+let a = prompt ('Ism kiriting ');
+let a1 = prompt ('Harf kiriting');
+
+
+
+
+
+    if (ism ()) { 
+     alert('Siz kiritgan ma\'lumot string tipida')
+ } else {
+  alert('String emas') 
+}
+ 
+
+
+
+
+
+// if (a 'ismini ichida' a1 'harifi mavjut emas'){
+    
+// }else(
+    
+// )
